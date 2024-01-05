c1cf0244bc3c0152c1c30e7e0f287984554cb641
ryanforsythe <forsytheryan@gmail.com>
2024-01-05 09:10:45 -0500

    Add files via upload
    MicroStrategy Rest API. Created due to the fact that the Workstation Content Group functionality doesn't do a great job of providing folder or GUID context.

--- Addto_ContentGroup_RestAPI_ReportData_JSON.py
+++ Addto_ContentGroup_RestAPI_ReportData_JSON.py
@@ -1,67 +1,76 @@
 #MicroStrategy Rest API
 #Addto_ContentGroup_RestAPI_ReportData_JSON
+#Created due to the fact that the Workstation Content Group functionality doesn't do a great job of providing folder or GUID context.
 
 import requests
 import json
 #from pandas.io.json import json_normalize
 import pandas as pd
-import numpy as np
 
-projectID = '92F117EF491CF3D633AB7982C831B065'
-#userName=input("User Name:")
-#pwd=input("Password:")
+serverName = '.cloud.microstrategy.com'
 userName=''
 pwd=''
-api_url = "https://altus-bpi-stage.agdata.net/MIApplications/api"
+projectID = '92F117EF491CF3D633AB7982C831B065'
+projectGUID = '92F117EF491CF3D633AB7982C831B065' #Source Dossier Project
+projectName = ''
+plaProjectName ='Platform Analytics'
+contentGroupID = '2A5B949544E2BE6511CCC898867F42A5'
+#userName=input("User Name:")
+#pwd=input("Password:")
+
+api_url = 'https://' + serverName + '/MicroStrategyLibrary/api'
 mstrAuth = {"username": userName, "password": pwd,"loginMode": 1, "maxSearch": 3}
 
-plaURL = "https://altus.agdata.net/AltusApplications/api"
-plaProjectID = 'DE3A4B4C4000D196AAD863A96EE97732'
-plaReportGUID = 'D388AA794807B1887AE9F1A63A8722D8'
+plaURL = api_url
+plauserName = userName
+plapwd = pwd
+plaReportGUID=''
 
 """
 dossierList = ["3E63CD1649B024748831059FA28E7FB6"
 ,"9FF25F8C44E4B54A596BC49A9588D818"
-,"8BED26434D504E485EABC3A7312A5912"
-,"1CB931A3408EC86BE3DBB6BA15604630"
-,"1554FB0E4ECF74733516C8A1136C6E33"
-,"479C678C4E92C0EA3CE54FB0E8E0AE82"
-,"A341263A40F2E1304AD359BB5D3389F9"
-,"00DF51664E035E9D427F37903EC8ADB8"
-,"C536CB954300E48F77A627B504E36A1B"
-,"D4C84A074A9CD4982D06CA9968306A22"
-,"F00F120747034F7BE03F919E78C5F3EF"
-,"4EB61E7A4E0CDFA3FD46E2B3E4C704DC"
-,"95A0D79140AF48C1929D19A015A86BC7"
 ]
 """
 
 def login(baseURL,username,password):
     header = {'username': username,
-            'password': password,
-            'loginMode': 1}
+              'password': password,
+              'loginMode': 1}
     r = requests.post(baseURL + '/auth/login', data=header)
     if r.ok:
         authToken = r.headers["x-mstr-authtoken"]
         cookies = dict(r.cookies)
         print("Token: " + authToken)
         print("Session ID: {}".format(cookies))
-        return authToken, cookies
+        headers_svr = {'X-MSTR-AuthToken': authToken,
+                       'Content-Type': 'application/json',
+                       'Accept': 'application/json'}
+        return authToken, cookies, headers_svr
     else:
         print("HTTP {} - {}, Message {}".format(r.status_code, r.reason, r.text))
         return []
+        
+def MSTRProject_api(api_url, headers_svr, cookies, proj_name):
+    projectProps = requests.get(api_url + '/projects/' + proj_name, headers=headers_svr, cookies=cookies)
+    project_dict = dict(projectProps.json())
+    projectGUID = project_dict["id"]
+    projectName = project_dict["name"]
+    print(projectGUID, projectName)
+
+    headers_prj = {'X-MSTR-AuthToken': authToken,
+                   'Content-Type': 'application/json',
+                   'Accept': 'application/json',
+                   'X-MSTR-ProjectID': projectGUID}
+
+    return projectGUID, headers_prj        
 
 #Get Platform Analytics Data
-authToken, cookies = login(plaURL,userName,pwd)
+authToken, cookies, headers_svr = login(plaURL,plauserName,plapwd)
+projectGUID, headers_prj = MSTRProject_api(plaURL, headers_svr, cookies, proj_name)    
 
 
 
-headers_rpt = {'X-MSTR-AuthToken': authToken,
-              'Content-Type': 'application/json',
-              'Accept': 'application/json',
-              'X-MSTR-ProjectID': plaProjectID}
-
-reportInstance = requests.post(plaURL + '/reports/' + plaReportGUID + '/instances', headers=headers_rpt, cookies=cookies)
+reportInstance = requests.post(plaURL + '/reports/' + plaReportGUID + '/instances', headers=headers_prj, cookies=cookies)
 print("Report Instance:" + str(reportInstance.status_code))
 report_dict = dict(reportInstance.json())
 #print(reportInstance)     
@@ -72,6 +81,7 @@ resultArray =[]
 
 i = 0
 while i < num_rows:
+       #Adjust based on report_dict results
       resultArray.append(report_dict["result"]["data"]["root"]["children"][0]["children"][i]["element"]["formValues"]["GUID"])
       i += 1
 
@@ -102,18 +112,15 @@ gridObjects = {
 
 #grid_obj = json.loads(gridData)
 
-#Use resultArray to populate Content Group
-authToken, cookies = login(api_url,userName,pwd)
-              
-headers_cg = {'X-MSTR-AuthToken': authToken,
-              'Content-Type': 'application/json',
-              'Accept': 'application/json'}
+#Add Dossier to Content Group
 
-contentGroupID = '2A5B949544E2BE6511CCC898867F42A5'
-contentGroup = requests.get(api_url + '/contentGroups/' + contentGroupID, headers=headers_cg, cookies=cookies)
+authToken, cookies, headers_svr = login(api_url,userName,pwd)
+              
+
+contentGroup = requests.get(api_url + '/contentGroups/' + contentGroupID, headers=headers_svr, cookies=cookies)
 print("Content Group:" + str(contentGroup.status_code))
 
-dossierList = resultArray
+dossierList = resultArray #comment out if using manual list.
 
 for d in dossierList:
 
@@ -121,7 +128,7 @@ for d in dossierList:
       "operationList": [
         {
           "op": "add",
-          "path": "/92F117EF491CF3D633AB7982C831B065",
+          "path": '/' + projectGUID,
           "value": [
             {"id": d,"type": 55}
           ],
@@ -130,10 +137,11 @@ for d in dossierList:
       ]
     }
 
-    response = requests.patch(api_url + '/contentGroups/' + contentGroupID + '/contents', headers=headers_cg, data=json.dumps(payload),cookies=cookies)
+    response = requests.patch(api_url + '/contentGroups/' + contentGroupID + '/contents', headers=headers_svr, data=json.dumps(payload),cookies=cookies)
     print("Content Group Add: " + d + " " + str(response.status_code))
     #print(response.reason)
     #print(response.text)
     
-responseCG = requests.get(api_url + '/contentGroups/' + contentGroupID + '/contents?projectId=' + projectID , headers=headers_cg,cookies=cookies)
+    
+responseCG = requests.get(api_url + '/contentGroups/' + contentGroupID + '/contents?projectId=' + projectID , headers=headers_svr,cookies=cookies)
     
